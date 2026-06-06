98e3f01ee25524000e120b13e778b78a8a0e2960
Fernando Toba Lopez <fetobalopez@gmail.com>
2026-06-05 23:46:47 -0300

    remove IA code

--- app/infrastructure/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Camada de infraestrutura.
-
-Aqui ficarão as implementações concretas de browser, proxies e persistência.
-"""
-
-# TODO: implementar provider de browser real usando Selenium ou Playwright.
-# TODO: implementar injeção de proxies no browser.
